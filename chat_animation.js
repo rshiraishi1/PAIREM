1f409a535274b322977305b1a89c68cf8a3255f5
rshiraishi1 <97076699+rshiraishi1@users.noreply.github.com>
2023-02-19 09:28:36 -0800

    Add files via upload

---
+++ chat_animation.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('form').on('submit', function (event) {
+      event.preventDefault();
+      var message = $('.message').first().clone();
+      message.find('p').text($('input').val());
+      message.prependTo('.chat-container');
+      $('input').val('');
+    });
+});
+
